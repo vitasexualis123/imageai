19ae930fa4646246ed9fff5c765b7ebb0cffe67b
vitasexualis123 <80819128+vitasexualis123@users.noreply.github.com>
2021-05-19 17:30:17 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,8 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
-
+let imageModelURL = 'https://vitasexualis123.github.io/imageai/';
 // Video
 let video;
 let flippedVideo;
